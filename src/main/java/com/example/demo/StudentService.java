83651451baf72ee3de07b3f281f2c693c38b2392
MaestroMichael <zapart.michal7@gmail.com>
2019-11-05 09:15:44 +0100

    Changed src

---
+++ src/main/java/com/example/demo/StudentService.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+
+import io.vavr.collection.List;
+
+public class StudentService {
+
+    List<Student> getStudents()
+    {
+//        return List.empty();
+        return this.students;
+    }
+    int id=0;
+    Student addStudent(NewStudent n_Student) {
+
+//        Student created=new Student(1,"Student1", "1-2-3", "IP");
+
+            id++;
+          int  id1= students.size() +1;
+        Student created=new Student(id1,n_Student.name, n_Student.numer,n_Student.grupa);
+        //return new Student(1,"aa","aa","aa");
+        students=students.prepend(created);
+        return created;
+
+    }
+
+    private List<Student> students = List.empty();
+
+
+}
